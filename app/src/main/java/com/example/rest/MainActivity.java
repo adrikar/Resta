ef06680025eb36c2060a3755b059091638f922da
me <orellanakaren30@gmail.com>
2020-12-04 14:34:49 -0400

    Correccion MainActivity

--- app/src/main/java/com/example/rest/MainActivity.java
+++ app/src/main/java/com/example/rest/MainActivity.java
@@ -10,14 +10,26 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+
     }
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();//logout
         startActivity(new Intent(getApplicationContext(),Login.class));
         finish();
+
     }
+
+
+
+
+
 }
