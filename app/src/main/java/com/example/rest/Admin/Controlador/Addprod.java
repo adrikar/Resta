323f85b1f46db6bf2a1b8f93b4e851a4c8f45c35
me <orellanakaren30@gmail.com>
2020-12-06 17:27:20 -0400

    Mitad de añadir producto

--- app/src/main/java/com/example/rest/Admin/Controlador/Addprod.java
+++ app/src/main/java/com/example/rest/Admin/Controlador/Addprod.java
@@ -1,5 +1,6 @@
 package com.example.rest.Admin.Controlador;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -18,7 +19,9 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.rest.Admin.MenuAdmin;
 import com.example.rest.Admin.modelA.Product;
+import com.example.rest.MainActivity;
 import com.example.rest.R;
 import com.example.rest.Register;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -31,8 +34,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.UUID;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class Addprod extends Fragment {
@@ -40,11 +47,11 @@ public class Addprod extends Fragment {
     Button aResgiterpBtn;
     ProgressBar aProgressBar;
     ImageView imageView;
-    Uri imageuri = null;
+
     Context context;
     StorageReference reference;
 
-    private static final int GALLERY_REQUEST=1;
+
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
@@ -68,7 +75,7 @@ public class Addprod extends Fragment {
 
         inicializarFirebase();
         reference = FirebaseStorage.getInstance().getReference();
-        openGallery();
+
 
         aResgiterpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,68 +102,44 @@ public class Addprod extends Fragment {
                     aCant.setError("Quantity is Required.");
                     return;
                 }
-                else if(!TextUtils.isEmpty(name)&&!TextUtils.isEmpty(price)&&imageuri!=null){
 
-                    StorageReference filepath = reference.child("producto_image").child(imageuri.getLastPathSegment());
-                    filepath.putFile(imageuri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                else if(!TextUtils.isEmpty(name)&&!TextUtils.isEmpty(price)){
+
+
+                    Product pro = new Product();
+                    pro.setProductId(UUID.randomUUID().toString());
+                    pro.setName(name);
+                    pro.setDescri(des);
+                    pro.setPrice(price);
+                    pro.setCant(cant);
+
+
+                    databaseReference.child("Product").child(pro.getProductId()).setValue(pro)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
-                                public void onSuccess(Uri uri) {
-                                    Uri downloaduri = uri;
-                                    final String img = downloaduri.toString();
-
-
-                                    Product pro = new Product();
-                                    pro.setProductId(UUID.randomUUID().toString());
-                                    pro.setName(name);
-                                    pro.setDescri(des);
-                                    pro.setPrice(price);
-                                    pro.setCant(cant);
-                                    pro.setImag(img);
-
-                                    databaseReference.child("Product").child(pro.getProductId()).setValue(pro)
-                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    Toast.makeText(context,"Success",Toast.LENGTH_SHORT).show();
-                                                }
-                                            }).addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(context, "Error ! " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                            aProgressBar.setVisibility(View.GONE);
-                                        }
-                                    });
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(context,"Success",Toast.LENGTH_SHORT).show();
 
                                 }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(context, "Error ! " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    aProgressBar.setVisibility(View.GONE);
+                                }
                             });
-                        }
-                    });
-                }
 
-                aProgressBar.setVisibility(View.VISIBLE);
+                }aProgressBar.setVisibility(View.VISIBLE);
+
 
             }
         });
 
-        return root;
+    return root;
     }
-
     private void inicializarFirebase() {
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference= firebaseDatabase.getReference();
-    }
-    private void openGallery(){
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(intent,GALLERY_REQUEST);
-            }
-        });
     }
 }
